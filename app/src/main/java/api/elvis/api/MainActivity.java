481d949004bb631c893c14b8a9b489791137cd65
elvis jose rosales <elvismaracaibomedellin@hotmail.com>
2023-05-15 16:24:39 -0500

    Prueba de API JSON --  https://jsonplaceholder.typicode.com/posts/1

---
+++ app/src/main/java/api/elvis/api/MainActivity.java
@@ -0,0 +1,77 @@
+package api.elvis.api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtUser, txtTitle, txtBody;
+    Button btnEnviar;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUser = findViewById(R.id.txtUser);
+        txtTitle = findViewById(R.id.txtTitle);
+        txtBody = findViewById(R.id.txtBody);
+        btnEnviar =findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LeerWs();
+
+            }
+        });
+
+
+    }
+
+    private void LeerWs (){
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject=new JSONObject(response);
+                    txtUser.setText(jsonObject.getString("userId"));
+                    String title = jsonObject.getString("title");
+                    txtTitle.setText(title);
+                    txtBody.setText(jsonObject.getString("body"));
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Volley.newRequestQueue(this).add(postResquest);
+    }
+
+
+}
